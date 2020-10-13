8b9b03c6ee443158c721f4c4ccbae42a2e52f55e
2nd-deck <archness@naver.com>
2020-10-13 19:54:50 +0900

    Updates

---
+++ precache-manifest.dacc4fd54925d7da2325c9ffc166aedf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "216a2e15c7d1862626c713dec63092ae",
+    "url": "/chunil/index.html"
+  },
+  {
+    "revision": "452f9a43996237ea268a",
+    "url": "/chunil/static/css/main.99f11218.chunk.css"
+  },
+  {
+    "revision": "ba6dc9e1a3a055d60017",
+    "url": "/chunil/static/js/2.cef429c5.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/chunil/static/js/2.cef429c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "452f9a43996237ea268a",
+    "url": "/chunil/static/js/main.a0b9b727.chunk.js"
+  },
+  {
+    "revision": "3a3389e475fb2a517f4e",
+    "url": "/chunil/static/js/runtime-main.a8fdead8.js"
+  }
+]);
