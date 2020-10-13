56cca8dde6cff9dc6a97ddd8ba2cf6a7ee9facac
2nd-deck <archness@naver.com>
2020-10-13 19:39:48 +0900

    Updates

---
+++ precache-manifest.1a22fc03212f0113a40ca2010e3020dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24213e403eddba82cdd8281680a63e05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "452f9a43996237ea268a",
+    "url": "/static/css/main.99f11218.chunk.css"
+  },
+  {
+    "revision": "383db8ae152126fae089",
+    "url": "/static/js/2.528cc6b9.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.528cc6b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "452f9a43996237ea268a",
+    "url": "/static/js/main.a0b9b727.chunk.js"
+  },
+  {
+    "revision": "57685025c738c7023605",
+    "url": "/static/js/runtime-main.7a4e93a0.js"
+  }
+]);
